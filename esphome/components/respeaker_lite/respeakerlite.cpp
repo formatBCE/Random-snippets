3b7dbe0af1a209ad453a965922423a6afa10a874
root <root@vscode>
2024-10-20 11:38:37 -0700

    Another take

--- esphome/components/respeaker_lite/respeakerlite.cpp
+++ esphome/components/respeaker_lite/respeakerlite.cpp
@@ -7,7 +7,7 @@ namespace respeakerlite {
 
 static const char* const TAG = "respeaker_lite";
 
-static const uint8_t RESPEAKER_LITE_MUTE_STATE = 0xF1;
+static const uint8_t RESPEAKER_LITE_MUTE_STATE = 0x81;
 
 float RespeakerLite::get_setup_priority() const {
   return setup_priority::IO;
@@ -17,19 +17,24 @@ void RespeakerLite::setup() {
   ESP_LOGI(TAG, "Setting up RespeakerLite...");
 }
 
+uint32_t now_time = 0;
+
 void RespeakerLite::loop() {
-  uint8_t data[1];
- 
-  if (this->read_register(RESPEAKER_LITE_MUTE_STATE, data, 1) != i2c::ERROR_OK) {
-    ESP_LOGE(TAG, "unable to read mute state");
-    this->mark_failed();
-    return;
-  }
-  bool new_mute_state = data[0] == 0x01;
-  if (this->mute_state_ != nullptr) {
-    if (!this->mute_state_->has_state() || (this->mute_state_->state != new_mute_state)) {
+  if (millis() - now_time > 1000) {
+    now_time = millis();
+
+    uint8_t data;
+    if (this->read_byte(RESPEAKER_LITE_MUTE_STATE, &data) != i2c::ERROR_OK) {
+      ESP_LOGE(TAG, "unable to read mute state");
+      this->mark_failed();
+      return;
+    }
+    bool new_mute_state = data == 0x01;
+    if (this->mute_state_ != nullptr) {
       ESP_LOGD(TAG, "RespeakerLite mute state: %d", new_mute_state);
-      this->mute_state_->publish_state(new_mute_state);
+      if (!this->mute_state_->has_state() || (this->mute_state_->state != new_mute_state)) {
+        this->mute_state_->publish_state(new_mute_state);
+      }
     }
   }
 }
