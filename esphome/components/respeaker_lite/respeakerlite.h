c699c21bfc68b551c4ea51bf32bae02530397d44
root <root@vscode>
2024-10-20 13:32:22 -0700

    Another take

--- esphome/components/respeaker_lite/respeakerlite.h
+++ esphome/components/respeaker_lite/respeakerlite.h
@@ -18,6 +18,34 @@ class RespeakerLite : public i2c::I2CDevice, public Component {
     this->mute_state_ = mute_state;
   }
 
+  uint8_t xmos_read_1byte(uint8_t resid, uint8_t cmd, uint8_t *value) {
+      return xmos_read_bytes(resid, cmd, value, 1);
+  }
+
+  uint8_t xmos_read_bytes(uint8_t resid, uint8_t cmd, uint8_t *value, uint8_t read_byte_num) {
+      if (read_byte_num == 0 || read_byte_num > 254) return 0xFF;
+      
+      uint8_t read_result;
+      std::vector<uint8_t> data_to_send = {resid, cmd, (uint8_t)(read_byte_num + 1)};
+      if (!this->write(data_to_send)) {
+          ESP_LOGE(TAG, "Failed to write to XMOS device.");
+          return 0xFF;
+      }
+
+      std::vector<uint8_t> read_data = this->read(read_byte_num + 1);
+      if (read_data.empty()) {
+          ESP_LOGE(TAG, "Failed to read from XMOS device.");
+          return 0xFF;
+      }
+
+      read_result = read_data[0];
+      for (uint8_t i = 0; i < read_byte_num; i++) {
+          value[i] = read_data[i + 1];
+      }
+
+      return read_result;
+  }
+
  protected:
   binary_sensor::BinarySensor* mute_state_{nullptr};
 };
