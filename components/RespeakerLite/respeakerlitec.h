140548d2c2bf1e57640f66911305bbe7a417dbe0
root <root@vscode>
2024-10-19 22:24:41 -0700

    Initial mute state component

---
+++ components/RespeakerLite/respeakerlitec.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <utility>
+#include "esphome/components/binary_sensor/binary_sensor.h"
+#include "esphome/components/i2c/i2c.h"
+#include "esphome/core/component.h"
+
+namespace esphome {
+namespace respeakerlitec {
+
+class RespeakerLiteC : public i2c::I2CDevice, public Component {
+ public:
+  void setup() override;
+  void loop() override;
+  float get_setup_priority() const override;
+
+  void set_mute_state(binary_sensor::BinarySensor* mute_state) {
+    this->mute_state_ = mute_state;
+  }
+
+ protected:
+  binary_sensor::BinarySensor* mute_state_{nullptr};
+};
+
+}  // namespace respeakerlitec
+}  // namespace esphome
