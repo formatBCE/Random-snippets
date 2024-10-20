53096fd0b3148e6e9a6557345d77daf4c8b39513
root <root@vscode>
2024-10-20 11:54:14 -0700

    Another take

--- esphome/components/respeaker_lite/respeakerlite.cpp
+++ esphome/components/respeaker_lite/respeakerlite.cpp
@@ -1,32 +1,55 @@
 #include "respeakerlite.h"
 #include "esphome/core/helpers.h"
 #include "esphome/core/log.h"
+#include "Wire.h"
 
 namespace esphome {
 namespace respeakerlite {
 
 static const char* const TAG = "respeaker_lite";
 
-static const uint8_t RESPEAKER_LITE_MUTE_STATE = 0x81;
+static const uint8_t RESPEAKER_LITE_MUTE_STATE = 0xF1;
 
 float RespeakerLite::get_setup_priority() const {
   return setup_priority::IO;
+}
+
+uint8_t xmos_read_1byte(uint8_t resid, uint8_t cmd, uint8_t *value) {
+    return xmos_read_bytes(resid, cmd, value, 1);
+}
+
+uint8_t xmos_read_bytes(uint8_t resid, uint8_t cmd, uint8_t *value, uint8_t read_byte_num) {
+    uint8_t read_result;
+    if (read_byte_num == 0 || read_byte_num > 254) return 0xFF;
+    read_byte_num++;
+    Wire.beginTransmission(0x42);
+    Wire.write(resid);
+    Wire.write(cmd);
+    Wire.write(read_byte_num);
+    Wire.endTransmission();
+
+    Wire.requestFrom(0x42, read_byte_num);
+    
+    read_result = Wire.read();
+    for (uint8_t i = 0; i < read_byte_num - 1; i++) {
+        value[i] = Wire.read();
+    }
+    
+    return read_result;
 }
 
 void RespeakerLite::setup() {
   ESP_LOGI(TAG, "Setting up RespeakerLite...");
 }
 
-uint32_t now_time = 0;
-
 void RespeakerLite::loop() {
-  uint8_t data;
-  if (this->read_byte(RESPEAKER_LITE_MUTE_STATE, &data) != i2c::ERROR_OK) {
+  uint8_t mute_status;
+  if (this->xmos_read_1byte(0xF1, 0x81, &mute_status); != i2c::ERROR_OK) {
     ESP_LOGE(TAG, "unable to read mute state");
     this->mark_failed();
     return;
   }
-  bool new_mute_state = data == 0x01;
+  bool new_mute_state = mute_status == 0x01;
   if (this->mute_state_ != nullptr) {
     ESP_LOGD(TAG, "RespeakerLite mute state: %d", new_mute_state);
     if (!this->mute_state_->has_state() || (this->mute_state_->state != new_mute_state)) {
