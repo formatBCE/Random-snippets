6148aedc806009e9fe6b317a4686e964cfe7893d
root <root@vscode>
2024-10-20 20:34:06 -0700

    Sensor for FW

--- esphome/components/respeaker_lite/__init__.py
+++ esphome/components/respeaker_lite/__init__.py
@@ -16,12 +16,14 @@ DEPENDENCIES = ['i2c']
 respeakerlite_ns = cg.esphome_ns.namespace('respeakerlite')
 RespeakerLite = respeakerlite_ns.class_('RespeakerLite', i2c.I2CDevice, cg.Component)
 CONF_MUTE_STATE= "mute_state"
+CONF_FIRMWARE_VERSION= "firmware_version"
 DEFAULT_ADDRESS = 0x42
 
 CONFIG_SCHEMA = cv.COMPONENT_SCHEMA.extend(
     {
         cv.GenerateID(): cv.declare_id(RespeakerLite),
         cv.Optional(CONF_MUTE_STATE): binary_sensor.binary_sensor_schema(),
+        cv.Optional(CONF_FIRMWARE_VERSION): sensor.sensor_schema(),
     }
 ).extend(i2c.i2c_device_schema(DEFAULT_ADDRESS))
 
@@ -33,3 +35,6 @@ async def to_code(config):
     if CONF_MUTE_STATE in config:
         mute_state = await binary_sensor.new_binary_sensor(config[CONF_MUTE_STATE])
         cg.add(var.set_mute_state(mute_state))
+    if CONF_FIRMWARE_VERSION in config:
+        firmware_version = await sensor.new_sensor(config[CONF_FIRMWARE_VERSION])
+        cg.add(var.set_firmware_version(firmware_version))
--- esphome/components/respeaker_lite/respeakerlite.h
+++ esphome/components/respeaker_lite/respeakerlite.h
@@ -2,6 +2,7 @@
 
 #include <utility>
 #include "esphome/components/binary_sensor/binary_sensor.h"
+#include "esphome/components/sensor/sensor.h"
 #include "esphome/components/i2c/i2c.h"
 #include "esphome/core/component.h"
 
@@ -18,8 +19,13 @@ class RespeakerLite : public i2c::I2CDevice, public Component {
     this->mute_state_ = mute_state;
   }
 
+  void set_firmware_version(sensor::Sensor* firmware_version) {
+    this->firmware_version_ = firmware_version;
+  }
+
  protected:
   binary_sensor::BinarySensor* mute_state_{nullptr};
+  sensor::Sensor* firmware_version_{nullptr};
 };
 
 }  // namespace respeakerlite
