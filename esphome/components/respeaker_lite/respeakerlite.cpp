6b166407ceb9c4f43a199722c7f3f21501c6a8c6
root <root@vscode>
2024-10-21 17:18:51 -0700

    nullptr

--- esphome/components/respeaker_lite/respeakerlite.cpp
+++ esphome/components/respeaker_lite/respeakerlite.cpp
@@ -14,39 +14,36 @@ float RespeakerLite::get_setup_priority() const {
 
 void RespeakerLite::setup() {
   ESP_LOGI(TAG, "Setting up RespeakerLite...");
+  this->get_firmware_version_();
 }
 
 unsigned long last_time = 0;
 const unsigned long interval = 1000;
 
 void RespeakerLite::get_firmware_version_() {
-  if (this->firmware_version_ == nullptr) {
-    return;
-  }
-  const uint8_t version_req[] = {0xF0, 0xD8, 4};
-  uint8_t version_resp[4];
+  if (this->firmware_version_ != nullptr) {
+    const uint8_t version_req[] = {0xF0, 0xD8, 4};
+    uint8_t version_resp[4];
 
-  auto error_code = this->write(version_req, sizeof(version_req));
-  if (error_code != i2c::ERROR_OK) {
-    ESP_LOGW(TAG, "Request version failed");
-    return;
-  }
+    auto error_code = this->write(version_req, sizeof(version_req));
+    if (error_code != i2c::ERROR_OK) {
+      ESP_LOGW(TAG, "Request version failed");
+      return;
+    }
 
-  error_code = this->read(version_resp, sizeof(version_resp));
-  if (error_code != i2c::ERROR_OK || version_resp[0] != 0) {
-    ESP_LOGW(TAG, "Read version failed");
-    return;
+    error_code = this->read(version_resp, sizeof(version_resp));
+    if (error_code != i2c::ERROR_OK || version_resp[0] != 0) {
+      ESP_LOGW(TAG, "Read version failed");
+      return;
+    }
+    
+    std::string version = str_sprintf("%u.%u.%u", version_resp[1], version_resp[2], version_resp[3]);
+    ESP_LOGI(TAG, "DFU version: %s", version.c_str());
+    this->firmware_version_->publish_state(version);
   }
-  
-  std::string version = str_sprintf("%u.%u.%u", version_resp[1], version_resp[2], version_resp[3]);
-  ESP_LOGI(TAG, "DFU version: %s", version.c_str());
-  this->firmware_version_->publish_state(version);
 }
 
 void RespeakerLite::get_mute_state_() {
-  if (this->mute_state_ == nullptr) {
-    return;
-  }
   uint8_t mute_req[3] = {0xF1, 0x81, 1};
   uint8_t mute_resp[2];
 
@@ -63,17 +60,15 @@ void RespeakerLite::get_mute_state_() {
   }
 
   bool new_mute_state = mute_resp[1] == 0x01;
-  if (!this->mute_state_->has_state() || (this->mute_state_->state != new_mute_state)) {
-    ESP_LOGI(TAG, "Mic mute state: %d", new_mute_state);
-    this->mute_state_->publish_state(new_mute_state);
+  if (this->mute_state_ != nullptr) {
+    if (!this->mute_state_->has_state() || (this->mute_state_->state != new_mute_state)) {
+      ESP_LOGI(TAG, "Mic mute state: %d", new_mute_state);
+      this->mute_state_->publish_state(new_mute_state);
+    }
   }
 }
 
 void RespeakerLite::loop() {
-  if (!this->firmware_version_->has_state()) {
-    this->get_firmware_version_();
-  }
-  
   unsigned long current_time = millis();
   if (current_time - last_time >= interval) {
     last_time = current_time;
