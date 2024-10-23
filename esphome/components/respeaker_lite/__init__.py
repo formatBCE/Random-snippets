fabf5a48b5d4b30be5f0e4b373b72810dd5cebe3
root <root@vscode>
2024-10-23 10:57:05 -0700

    import

--- esphome/components/respeaker_lite/__init__.py
+++ esphome/components/respeaker_lite/__init__.py
@@ -1,6 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome import automation
+from esphome.automation import maybe_simple_id
 from esphome.components import binary_sensor, i2c, text_sensor
 from esphome.const import (
     CONF_ID,
